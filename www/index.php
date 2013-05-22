6839287c0072bbeeccfdf9b6d6e709b76ea7bdf7
tarentio62 <*dragoves62>
2013-05-22 17:02:12 +0200

    tp

---
+++ www/index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Mes Bd</title>
+<link rel="stylesheet" href="jss/style.css">
+<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+<div class="wrapper">
+
+<br class="clear">
+
+<a id="header" href="index.php"><img class="logo" src="img/logo-72.png">
+
+<div class="titre">
+
+<h1>MyBd.fr</h1><h2> plein d'infos sur les BD</h2></div>
+</a>
+</div>
+
+<div id="menu" class="wrapper">
+<br class="clear">
+<ul>
+<li><a href="search.php">Recherche</a></li>
+<li><a href="last.php">lAST</a></li>
+<li><a href="random.php">random</a></li>
+<li><a href="legal.php">Legal</a></li>
+</ul>
+</div>
+
+<div class="footer">
+<div id="menufooter">
+<br class="clear">
+<ul>
+<li><a href="search.php">Recherche</a></li>
+<li><a href="last.php">lAST</a></li>
+<li><a href="random.php">random</a></li>
+<li><a href="legal.php">Legal</a></li>
+</ul>
+</div>
+</div>
+</body>
+</html>
---
+++ www/search.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<title>Mes Bd</title>
+<link rel="stylesheet" href="jss/style.css">
+<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+<div class="wrapper">
+<br class="clear">
+<a id="header" href="index.php"><img class="logo" src="img/logo-72.png">
+<div class="titre">
+<h1>MyBd.fr</h1><h2> plein d'infos sur les BD</h2>
+</div>
+</a>
+<div id="menu">
+<br class="clear">
+<ul>
+<li><a href="search.php">Recherche</a></li>
+<li><a href="last.php">lAST</a></li>
+<li><a href="random.php">random</a></li>
+<li><a href="legal.php">Legal</a></li>
+</ul>
+<br>
+</div>
+</div>
+<div class="wrapper">
+<div id="contenu">
+<form>
+<br>
+<label>Auteur</label><input id="auteur" type="text">
+<br>
+<label>Nationalitée</label><input id="nationalite" type="text">
+<br>
+<label>Titre</label><input id="titre" type="text">
+<br>
+<label>Annee</label><input id="annee" type="date">
+<br>
+<label>Prix</label><input id="prix" type="number">
+<br>
+<label>Disponibilité</label><input id="disponibilite" type="checkbox">
+<br>
+</form>
+</div>
+</div>
+<div class="wrapper">
+<div class="footer">
+<div id="menufooter">
+<br class="clear">
+<ul>
+<li><a href="search.php">Recherche</a></li>
+<li><a href="last.php">lAST</a></li>
+<li><a href="random.php">random</a></li>
+<li><a href="legal.php">Legal</a></li>
+</ul>
+</div>
+</div>
+</div>
+</body>
+</html>
